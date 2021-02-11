127fafb236e8dbb06198d1b3fad7c4ae51781f6f
Marcel Klehr <mklehr@gmx.net>
2021-02-11 15:05:45 +0100

    Add dummy completion URL

--- src/magpie.config.js
+++ src/magpie.config.js
@@ -1,5 +1,6 @@
 export default {
   experimentId: '172',
   submissionUrl: 'https://magpie-demo.herokuapp.com/api/submit_experiment/172',
+  completionUrl: 'https://app.prolific.co/submissions/complete?cc=I2PWSFRG',
   contactEmail: 'test@random.com'
 };
