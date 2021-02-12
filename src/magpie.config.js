531caca41375a477c95f27103b0fb596ad754d80
Marcel Klehr <mklehr@gmx.net>
2021-02-12 13:20:04 +0100

    config: set prolific mode

--- src/magpie.config.js
+++ src/magpie.config.js
@@ -2,5 +2,6 @@ export default {
   experimentId: '172',
   submissionUrl: 'https://magpie-demo.herokuapp.com/api/submit_experiment/172',
   completionUrl: 'https://app.prolific.co/submissions/complete?cc=I2PWSFRG',
-  contactEmail: 'test@random.com'
+  contactEmail: 'test@random.com',
+  mode: 'prolific'
 };
