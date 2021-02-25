4c3a09be0d0c846eedc8c3b1729c180b4b6ac9b7
michael-franke <mchfranke@gmail.com>
2021-02-25 20:19:06 +0100

    going live again

--- src/magpie.config.js
+++ src/magpie.config.js
@@ -3,5 +3,5 @@ export default {
   serverUrl: 'https://mcmpact.ikw.uni-osnabrueck.de/magpie/',
   completionUrl: 'https://app.prolific.co/submissions/complete?cc=681A8813',
   contactEmail: 'michael.franke@uni-osnabrueck.de',
-  mode: 'directLink'
+  mode: 'prolific'
 };
